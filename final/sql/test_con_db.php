142a081d7e9d43d45270d3105175585a48bd5675
toeysp130 <choptum@hotmail.co.th>
2021-03-18 10:46:30 +0700

    commit

--- final/sql/test_con_db.php
+++ final/sql/test_con_db.php
@@ -1,12 +1,22 @@
 <?php
 $servername = "localhost";
 $username = "root";
-$password = "0";
+$password = "";
 // Create connection
 $conn = new mysqli($servername, $username, $password);
 // Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
-echo "Connected successfully";
+//echo "Connected successfully";
+
+$sql = "CREATE DATABASE myDB";
+if (mysqli_query($conn, $sql)) {
+    echo "Database created successfully";
+} else {
+    echo "Error creating database: " . mysqli_error($conn);
+}
+mysqli_close($conn);
+
+
 ?>
